f3d2e25a5abae523e2f3e7c709f50dd271ec5fd2
SOPHAT <freedomearning123@gmail.com>
2025-10-18 10:39:54 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <FirstMessage/>
+</template>
+
+<script>
+import FirstMessage from './components/FirstMessage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstMessage
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: black;
+  }
+
+</style>
---
+++ src/components/firstMessage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>This is my first Vuejs project</p>
+    </div>
+</template>
