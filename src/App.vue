72c9149a5a24e02ef32bd74171415f04d0fd4cf8
SOPHAT <freedomearning123@gmail.com>
2025-10-26 16:28:08 +0700

    category display completed

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <template>
-  <FirstMessage/>
+
+  <div class="category-display">
+    <Category v-for="category in categories" :key="category.categoryName" :image="category.image" :categoryName="category.categoryName" :num="category.num" :bgColor="category.bgColor"/>
+  </div>
+  
 </template>
 
 <script lang="ts">
-import FirstMessage from './components/FirstMessage.vue'
+import FirstMessage from './components/firstMessage.vue'
+import Category from './components/category.vue'
 
 export default {
   name: 'App',
   components: {
-    FirstMessage
+    FirstMessage,
+    Category
+  },
+
+  data() {
+    return {
+      categories: [
+        { categoryName: 'Cake & Milk', num: 14, bgColor: '#F2FCE4', image: '/images/burger.png' },
+        { categoryName: 'Peach', num: 17, bgColor: '#FFFCEB', image: '/images/peach.png' },
+        { categoryName: 'Organic Kiwi', num: 21, bgColor: '#ECFFEC', image: '/images/kiwi.png' },
+        { categoryName: 'Red Apple', num: 68, bgColor: '#FEEFEA', image: '/images/apple.png' },
+        { categoryName: 'Snack', num: 34, bgColor: '#FFF3EB', image: '/images/snack.png' },
+        { categoryName: 'Black plum', num: 25, bgColor: '#FFF3FF', image: '/images/blackplum.png' },
+        { categoryName: 'Vegetables', num: 65, bgColor: '#F2FCE4', image: '/images/vegetables.png' },
+        { categoryName: 'Headphone', num: 33, bgColor: '#FFFCEB', image: '/images/headphone.png' },
+        { categoryName: 'Cake & Milk', num: 54, bgColor: '#F2FCE4', image: '/images/cake&milk.png' },
+        { categoryName: 'Orange', num: 63, bgColor: '#FFF3FF', image: '/images/orange.png' }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-  body {
-    background-color: black;
+<style scoped>
+
+  .category-display {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 24px;
   }
 
 </style>
---
+++ src/components/category.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="box" :style="{ backgroundColor : bgColor}">
+        <img id="category-image" :src="image" alt="">
+        <p id="category-name">{{ categoryName }}</p>
+        <p id="category-num">{{ num }} items</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props: {
+        categoryName: {
+            type: String,
+            required: true
+        },
+        num: {
+            type: Number,
+            required: true
+        },
+
+        bgColor: {
+            type: String,
+            default: '#ffffff'
+        },
+        image: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style>
+    .box {
+        width: 136px;
+        height: 185px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+    }
+    #category-image {
+        width: 120px;
+        height: 120px;
+    }
+    #category-name {
+        font-family: 'Quicksand', sans-serif;
+        font-size: 16px;
+        color: rgba(37, 61, 78, 1);
+    }
+
+    #category-num {
+        font-family: 'Lato', sans-serif;
+        font-size: 12px;
+        color: rgba(182, 182, 182, 1);
+    }
+</style>
